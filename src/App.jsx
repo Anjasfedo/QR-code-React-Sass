ca741c6aa5f618310493d2bdcae4b5808ba3b1f4
G1A021037-ANJASFEDO <fedoaf98@gmail.com>
2023-10-05 09:37:46 +0000

    Done?

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,15 @@
-import './App.css'
+import './App.scss'
 
 export default function App() {
   return (
     <main>
-      React ⚛️ + Vite ⚡ + Replit 🌀
+      <div className="container">
+        <img src="public/image-qr-code.png"></img>
+        <div>
+          <h1>Improve your front-end skills by buildings projects</h1>
+          <p>Scan the QR code to visit FrontEnd Mentor and take your coding skills to the next level</p>
+        </div>
+      </div>
     </main>
   )
 }
