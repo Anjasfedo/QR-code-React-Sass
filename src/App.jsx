496e32c51fd89453d3a9f2a9a99da5f8c7838337
G1A021037-ANJASFEDO <fedoaf98@gmail.com>
2023-10-06 07:36:19 +0000

    import img

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
-import './App.scss'
+import './App.scss';
+import qr from "../public/image-qr-code.png";
 
 export default function App() {
   return (
     <main>
       <div className="container">
-        <img src="public/image-qr-code.png"></img>
+        <img src={qr}></img>
         <div>
           <h1>Improve your front-end skills by buildings projects</h1>
           <p>Scan the QR code to visit FrontEnd Mentor and take your coding skills to the next level</p>
